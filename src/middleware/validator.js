426f8a7b709ab2a481bb23cab496bc1d2ea43820
AnneThor <anne.thor@gmail.com>
2021-02-25 21:06:51 -0800

    updated validator to use next() to pass error; updated linter script in package.json

--- src/middleware/validator.js
+++ src/middleware/validator.js
@@ -2,7 +2,7 @@
 
 module.exports = (req, res, next) => {
   if (!req.query.name) {
-    throw new Error('Request must have a name property');
+    next('Request must have a name property');
   }
   next();
 };
